5ad9c486a209f23ecb91309e0ca9deb08a39bcb6
nin3lee <2091392083@qq.com>
2023-12-27 19:25:57 +0800

    update

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    // 示例：允许来自单个域名的远程图像优化。
+    domains: ['https://7bu.top/user/images']
+  },
+  integrations: [tailwind()]
+});
---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../.astro/types.d.ts" />
+// 这是TypeScript的环境类型提示文件
+/// <reference types="astro/client" />
